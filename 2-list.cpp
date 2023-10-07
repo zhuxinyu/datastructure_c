252891f391e809d76108a6fde724d38f35df9830
rice <81219723@qq.com>
2023-10-07 17:17:21 +0800

    单链表的操作

--- 2-list.cpp
+++ 2-list.cpp
@@ -95,7 +95,7 @@ int getElem(Sqlist L, int p, int &e) // 要改变，所以用引用型
         return 0;
     }
     e = L.data[p];
-    return 1
+    return 1;
 }
 
 // 5
@@ -115,7 +115,162 @@ int deleteElem(Sqlist &L, int p, int &e) // 需要改变的变量用引用型
     return 1;     // 删除成功，返回1
 }
 
-/*单链表的操作*/
+/*单链表的操作（默认是带头结点的链表）
+ * 1. 建表（尾插法）
+ * 2. 建表（头插法）
+ * 3. 合并两个单链表（升序）
+ * 4. 合并两个单链表（降序）
+ * 5. 查找及删除结点
+ */
+
+// 1
+void createllistR(LNode *&C, int a[], int n) // 要改变的变量用引用型
+{
+    LNode *s, *r; // s用来指向新申请的结点，r始终指向C的终端结点
+    int i;
+    C = (LNode *)malloc(sizeof(LNode)); // 申请C的头结点空间
+    C->next = NULL;
+    r = C;                  // r指向头结点，因为此时头结点就是终端结点
+    for (i = 0; i < n; ++i) // 循环申请n个结点来接收数组a中的元素
+    {
+        s = (LNode *)malloc(sizeof(LNode)); // s指向新申请的结点
+        s->data = a[i];                     // 用新申请的结点来接收a中的一个元素
+        r->next = s;                        // 用r来接纳新结点
+        r = r->next;                        // r指向终端结点，以便于接纳下一个到来的结点
+    }
+    r->next = NULL; // 数组a中所有的元素都已经装入链表C中， C的终端结点的指针域置为NULL，C建立完成
+}
+
+// 2
+void createlistF(LNode *&C, int a[], int n)
+{
+    LNode *s;
+    int i;
+    C = (LNode *)malloc(sizeof(LNode));
+    C->next = NULL;
+    for (i = 0; i < n; ++i)
+    {
+        s = (LNode *)malloc(sizeof(LNode));
+        s->data = a[i];
+        // 下边两句是头插法的关键步骤
+        s->next = C->next; // s所指新结点的指针域next指向C中的开始结点
+        C->next = s;       // 头结点的指针域next指向s结点，使得s成为新的开始结点
+    }
+}
+
+// 3
+void mergeA(LNode *A, LNode *B, LNode *&C)
+{
+    LNode *p = A->next; // p来跟踪A的最小值结点
+    LNode *q = B->next; // q来跟踪B的最小值结点
+    LNode *r;           // r始终指向C的终端结点
+    C = A;              // 用A的头结点来做C的头结点
+    C->next = NULL;
+    free(B);                       // B的头结点已无用，则释放掉
+    r = C;                         // r指向C，因为此时头结点也是终端结点
+    while (p != NULL && q != NULL) // 当p与q都不空时没选取p与q所指结点中的较小者插入C的尾部
+    {
+        // 尾插法思想
+        if (p->data <= q->data)
+        {
+            r->next = p;
+            p = p->next;
+            r = r->next;
+        }
+        else
+        {
+            r->next = q;
+            q = q->next;
+            r = r->next;
+        }
+    }
+    r->next = NULL;
+    if (p != NULL)
+    {
+        r->next = p;
+    }
+
+    if (q != NULL)
+    {
+        r->next = q;
+    }
+}
+
+// 4
+void merge(LNode *A, LNode *B, LNode *&C)
+{
+    LNode *p = A;
+    LNode *q = B;
+    LNode *s;
+    C = A;
+    C->next = NULL;
+    free(B);
+    while (p != NULL && q != NULL)
+    {
+        // 头插法思想
+        if (p->data <= q->data)
+        {
+            s = p;
+            p->next = C->next;
+            C->next = s;
+        }
+        else
+        {
+            s = q;
+            q = q->next;
+            s->next = C->next;
+            C->next = s;
+        }
+    }
+
+    /*下边这两个循环是和求递增归并序列不同的地方，必须将剩余元素逐个插入C的头部才能得到最终的递减序列*/
+    while (p != NULL)
+    {
+        s = p;
+        p = p->next;
+        s->next = C->next;
+        C->next = s;
+    }
+
+    while (q != NULL)
+    {
+        s = q;
+        q = q->next;
+        s->next = C->next;
+        C->next = s;
+    }
+}
+
+// 5
+
+int findAndDeleteL(LNode *C, int x)
+{
+    LNode *p, *q;
+    p = C;
+    /*查找部分开始*/
+    while (p->next != NULL)
+    {
+        if (p->next->data == x)
+        {
+            break;
+        }
+        p = p->next;
+    }
+    /*查找部分结束*/
+    if (p->next == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        /*删除部分开始*/
+        q = p->next;
+        p->next = p->next->next;
+        free(q);
+        /*删除部分结束*/
+        return 1;
+    }
+}
 
 /*双链表的操作*/
 
